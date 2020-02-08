5c617412458e4a70203131ee83bd870ea19b1baa
hanxuzhao <hanxuzhao@5i5j.com>
2020-02-09 00:13:34 +0800

    master

--- model/src/test/java/A.java
+++ model/src/test/java/A.java
@@ -1,8 +1,8 @@
 public class A {
 
     public static void main(String[] args){
-    System.out.println("999999-dev");
-    System.out.println("dev");
+    System.out.println("999999-master");
+    System.out.println("master");
 
     }
 
